a5d49864715cc0dae92a5ed9bd206c6f75f14b04
blainefreestone <blainefreestone01@gmail.com>
2024-02-26 15:31:16 -0800

    Added home page for popular stocks and explanation of the website.

--- app/controllers/stock_prices_controller.rb
+++ app/controllers/stock_prices_controller.rb
@@ -4,8 +4,7 @@ class StockPricesController < ApplicationController
       stock_price_in_dollar = StockPrice.get_stock_data(params[:search])["results"].last["c"]
       @stock_price = StockPrice.new(ticker: params[:search], current_price_cents: (stock_price_in_dollar * 100).to_i)
     else
-      stock_price_in_dollar = StockPrice.get_stock_data("AAPL")["results"].last["c"]
-      @stock_price = StockPrice.new(ticker: "AAPL", current_price_cents: stock_price_in_dollar * 100)
+      render 'home_page'
     end
   end
 end
